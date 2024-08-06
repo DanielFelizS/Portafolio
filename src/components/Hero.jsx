56e77356f24815a818bab5d953961717815d569a
DANIEL  FELIZ  SANTANA <a19a05048@educacion.edu.do>
2024-08-05 21:18:02 -0400

    Agregando el icono de Youtube

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,9 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
-import { DiGithubBadge } from "react-icons/di";
-import { FaLinkedin } from "react-icons/fa";
+// import { DiGithubBadge } from "react-icons/di";
+import { FaLinkedin, FaYoutube } from "react-icons/fa";
+import { BsGithub } from "react-icons/bs";
 import { MdEmail } from "react-icons/md";
 import { useTranslation } from "react-i18next";
 
@@ -14,6 +15,7 @@ export default function Hero ({currentLanguage})  {
     const DownloadCV = () => {
       window.open(CV, '_blank');
     };
+    
   return (
     <>
     <div className="hero-container" id="about-me">
@@ -28,18 +30,19 @@ export default function Hero ({currentLanguage})  {
       {currentLanguage === 'en' ? t('Hero.btnCV') : t('Hero.btnCvEs')}
       </button>
       <a href="https://github.com/DanielFelizS" target="_blank" className="hero-icon-link">
-        <DiGithubBadge className="hero-icon github-icon" />
+        <BsGithub className="hero-icon github-icon" />
+      </a>
+      <a href="https://www.youtube.com/channel/UCyzzGyR9DKHmw8C3C0f3mRw" target="_blank" className="hero-icon-link">
+        <FaYoutube className="hero-icon red-icon"/>
       </a>
       <a href="https://www.linkedin.com/in/daniel-f%C3%A9liz/" target="_blank" className="hero-icon-link">
         <FaLinkedin className="hero-icon linkedin-icon" />
       </a>
       <a href="mailto:felizsdaniel0@gmail.com" target="_blank" className="hero-icon-link">
-        <MdEmail className="hero-icon email-icon" />
+        <MdEmail className="hero-icon red-icon" />
       </a>
     </div>
   </div>
-  
- 
   </div>
   <div className="hero-imagen">
     <img src="/img/Daniel_Feliz.jpeg" alt="Daniel Feliz" className="hero-image" />
